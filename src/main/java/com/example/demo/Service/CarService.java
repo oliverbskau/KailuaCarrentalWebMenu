eb93ba45fecb4dfb3780ddff47b4738d31a82dec
jona881a <jonaskunert@hotmail.com>
2021-04-23 13:39:28 +0200

    Lavet Car

--- src/main/java/com/example/demo/Service/CarService.java
+++ src/main/java/com/example/demo/Service/CarService.java
@@ -1,4 +1,35 @@
 package com.example.demo.Service;
 
+import com.example.demo.Model.Car;
+import com.example.demo.Repository.CarRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CarService {
+
+    @Autowired
+    CarRepo carRepo;
+
+    public List<Car> fetchAll() {
+        return null;
+    }
+
+    public Car addCar(Car car) {
+        return null;
+    }
+
+    public Car findCarById(int carid) {
+        return null;
+    }
+
+    public Boolean deleteCar(int carid) {
+        return null;
+    }
+
+    public Car updateCar(int carid, Car car) {
+        return null;
+    }
 }
