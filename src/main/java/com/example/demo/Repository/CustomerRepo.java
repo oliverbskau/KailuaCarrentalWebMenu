3bb5a3ae4c2c89eac2b0817ec6214762a943034b
jona881a <jonaskunert@hotmail.com>
2021-04-23 15:08:55 +0200

    Lavet customer repo og service

--- src/main/java/com/example/demo/Repository/CustomerRepo.java
+++ src/main/java/com/example/demo/Repository/CustomerRepo.java
@@ -1,6 +1,57 @@
 package com.example.demo.Repository;
 
+import com.example.demo.Model.Contract;
+import com.example.demo.Model.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public class CustomerRepo {
+
+    @Autowired
+    JdbcTemplate template;
+
+    public List<Customer> fetchAll() {
+        String sql = "SELECT * FROM customers";
+        RowMapper<Customer> customers = new BeanPropertyRowMapper<>(Customer.class);
+
+        return template.query(sql,customers);
+    }
+
+    public Customer addCustomer(Customer c) {
+        String sql = "INSERT INTO customers(first_name,last_name,phonenumber,email,drivers_license_number,address,zipcode) " +
+                "VALUES(?,?,?,?,?,?,?,?)";
+        template.update(sql,c.getFirst_name(),c.getLast_name(),c.getPhonenumber(),c.getEmail(),
+                c.getDriver_license_number(), c.getAddress(),c.getZipcode());
+
+        return null;
+    }
+
+    public Customer findCustomerById(int customerid) {
+        String sql = "SELECT * FROM customers WHERE customerid = ?";
+        RowMapper<Customer> customers = new BeanPropertyRowMapper<>(Customer.class);
+        Customer c = template.queryForObject(sql, customers, customerid);
+
+        return c;
+    }
+
+    public Boolean deleteCustomer(int customerid) {
+        String sql = "DELETE FROM customers WHERE customerid = ?";
+
+        return template.update(sql,customerid) < 0;
+    }
+
+    public Customer updateCustomer(int customerid, Customer c) {
+        String sql = "UPDATE customers SET first_name = ?, last_name = ?,phonenumber = ?,email = ?" +
+                ",drivers_license_number = ?,address = ?,zipcode = ?";
+        template.update(sql,c.getFirst_name(),c.getLast_name(),c.getPhonenumber(),c.getEmail(),
+                c.getDriver_license_number(), c.getAddress(),c.getZipcode());
+        return null;
+    }
 
 }
--- src/main/java/com/example/demo/Service/CustomerService.java
+++ src/main/java/com/example/demo/Service/CustomerService.java
@@ -1,4 +1,35 @@
 package com.example.demo.Service;
 
+import com.example.demo.Model.Customer;
+import com.example.demo.Repository.CustomerRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CustomerService {
+
+    @Autowired
+    CustomerRepo customerRepo;
+
+    public List<Customer> fetchAll() {
+        return customerRepo.fetchAll();
+    }
+
+    public Customer addCustomer(Customer c) {
+        return customerRepo.addCustomer(c);
+    }
+
+    public Customer findCustomerById(int customerid) {
+        return customerRepo.findCustomerById(customerid);
+    }
+
+    public Boolean deleteCustomer(int customerid) {
+        return customerRepo.deleteCustomer(customerid);
+    }
+
+    public Customer updateContract(int customerid, Customer c) {
+        return customerRepo.updateCustomer(customerid,c);
+    }
 }
